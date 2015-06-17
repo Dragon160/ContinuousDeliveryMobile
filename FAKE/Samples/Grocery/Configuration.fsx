b0d16c6b16341589dcf64d203993170383c4e7cb
Adrian <adrian.seceleanu@zuehlke.com>
2015-06-17 09:52:52 +0200

    Fixed load ref

--- FAKE/Samples/Grocery/Configuration.fsx
+++ FAKE/Samples/Grocery/Configuration.fsx
@@ -1,18 +1,35 @@
-#load "../ContinuousMobileDelivery.fsx"
+#load "ContinuousMobileDelivery.fsx"
+
 open ContinuousMobileDelivery
 
 type Grocery() =
 
     let androidApp config = (Android, Project "Grocery.Android.csproj", config)
+
     let iOSApp config = (IOS, Project "Grocery.IOS.csproj", config)
-    let unitTests = (Project "Grocery.UnitTests.csproj", "Test", "Grocery.UnitTests.dll")
+
+    let unitTests = (Project "Grocery.UnitTests.csproj", "Test", "Grocery.UnitTests.dll") // dll name kann evtl aus Projektdatei gelesen werden
+
     let uiTests = (Project "Grocery.UITests.csproj", "UITest", "Grocery.UITests.dll")
 
     interface IConfiguration with
-        member this.Build = [| androidApp "Debug"; iOSApp "Debug" |]
-        member this.UnitTest = [| (unitTests, "testresults.xml") |]
-        member this.UITest = [| (androidApp "Release", uiTests, "uitestresults.xml") |]
+
+        member this.Build = [| 
+            androidApp "Debug"; 
+            iOSApp "Debug" 
+            |]
+
+        member this.UnitTest = [| 
+            (unitTests, "testresults.xml") 
+            |]
+
+        member this.UITest = [| 
+            (androidApp "Release", uiTests, "uitestresults.xml") ;
+            |]
+
         member this.Package = Skip
+
         member this.Publish = Skip
 
+let configurarion = Grocery()
 
